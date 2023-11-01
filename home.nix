315f8693e1486a0c33815edce4d43c1c257f25e8
waltz <79410846+Wa1t5@users.noreply.github.com>
2023-11-01 19:00:38 -0300

    update git config & add nt-config.nix

--- home.nix
+++ home.nix
@@ -67,13 +67,15 @@
     };
 
 
-    programs = {
-    
+    programs = {    
         # Git
         git = {
             enable = true;
             userName = "waltz";
             userEmail = "79410846+Wa1t5@users.noreply.github.com";
+            signing = {
+                key = "0x0EEB210433A61B10";
+            };
         };
         
         # Waybar
