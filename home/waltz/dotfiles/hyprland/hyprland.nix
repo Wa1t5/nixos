8e907ffc190853352a4aa966d7ebc4042f180ac6
Waltz <79410846+Wa1t5@users.noreply.github.com>
2024-06-02 02:27:29 -0300

    Revert "Disable hyprland and enable gnome (1)"
    This reverts commit 022a35f2f3bec1e3ad0554e19e61453b3d268176.

--- home/waltz/dotfiles/hyprland/hyprland.nix
+++ home/waltz/dotfiles/hyprland/hyprland.nix
@@ -2,7 +2,7 @@
 {  
         # Hyprland
         wayland.windowManager.hyprland = {
-            enable = false;
+            enable = true;
             package = inputs.hyprland.packages.${pkgs.system}.hyprland;            
             systemd.enable = true;
             xwayland.enable = true;
